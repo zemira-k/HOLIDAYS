3ea43b91b1de9d25f99245e540c522c8aff176e6
AlmogSkalka <almogsk14@gmail.com>
2023-01-16 16:54:14 +0200

    removed API url to avoid candle lighting as its not a holiday

--- holiday-app/src/Pages/MainPage.js
+++ holiday-app/src/Pages/MainPage.js
@@ -7,7 +7,7 @@ export default function MainPage() {
     startDate = "2023-01-01";
     endDate = "2023-12-31";
     try {
-      const url = `https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&start=${startDate}&end=${endDate}&geonameid=281184`;
+      const url = `https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&start=${startDate}&end=${endDate}`;
       const response = await axios.get(url);
       console.log("jewish holidays in range: ", response.data);
     } catch (error) {
