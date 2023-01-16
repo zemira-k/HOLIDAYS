f5edaa73347a6e038def29fa837d995bc7e506f7
AlmogSkalka <almogsk14@gmail.com>
2023-01-16 17:50:13 +0200

    Getting dates to GetHebrewDates and rendering the data

--- holiday-app/src/Pages/MainPage.js
+++ holiday-app/src/Pages/MainPage.js
@@ -2,22 +2,65 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function MainPage() {
+  const [HolidaysInRange, setHolidaysInRange] = useState([]);
+  const [StartDate, setStartDate] = useState("");
+  const [EndDate, setEndDate] = useState("");
+
   const GetHebrewDates = async (startDate, endDate) => {
     // Assuming startDate & endDate are YYYY-MM-DD string formated
-    startDate = "2023-01-01";
-    endDate = "2023-12-31";
     try {
       const url = `https://www.hebcal.com/hebcal?v=1&mod=on&cfg=json&maj=on&min=on&start=${startDate}&end=${endDate}`;
       const response = await axios.get(url);
-      console.log("jewish holidays in range: ", response.data);
+      setHolidaysInRange(response.data.items);
     } catch (error) {
       console.error(error);
     }
   };
 
-  useEffect(() => {
-    GetHebrewDates("", "");
-  }, []);
+  const StartDateChanged = (event) => {
+    setStartDate(event.target.value);
+  };
 
-  return <div>MainPage</div>;
+  const EndDateChanged = (event) => {
+    setEndDate(event.target.value);
+  };
+
+  const SearchHolidays = () => {
+    // DIANA --> here I would validate that startDate is earlier then endDate
+    GetHebrewDates(StartDate, EndDate);
+  };
+  const ResetData = () => {
+    setHolidaysInRange([]);
+  };
+  return (
+    <div>
+      <h1>DATES FORMAT "YYYY-MM-DD"</h1>
+      <input
+        type="text"
+        placeholder="Start Date"
+        onChange={StartDateChanged}
+      ></input>
+      <input
+        type="text"
+        placeholder="End Date"
+        onChange={EndDateChanged}
+      ></input>
+      <button style={{ background: "green" }} onClick={SearchHolidays}>
+        SEARCH HOLIDAYS
+      </button>
+      <button style={{ background: "red" }} onClick={ResetData}>
+        RESET
+      </button>
+      <br />
+
+      {Object.keys(HolidaysInRange).length
+        ? HolidaysInRange.map((item, ind) => (
+            <div id={ind}>
+              Holiday: {item.title}, Date: {item.date} <br />
+            </div>
+          ))
+        : "There are no holidays"}
+      <br />
+    </div>
+  );
 }
