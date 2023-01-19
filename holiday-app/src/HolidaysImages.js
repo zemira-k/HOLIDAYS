3161b415f4c88a488a6206c470793f4a17ccdb6a
unknown <moshe89891@gmail.com>
2023-01-19 17:57:24 +0200

    Created the card grid with the presentation of the data inside

---
+++ holiday-app/src/HolidaysImages.js
@@ -0,0 +1,19 @@
+let ImageUrl = {
+  HaAliyah:"https://kanstatic.azureedge.net/download/pictures/2021/8/9/imgid=58935_B.jpeg",
+  Pesach: "https://assets-global.website-files.com/5b8fd783bee52c8fb59b1fac/5ce2c7ac340b30151c9ac775_TheTorah.com%252FPesach-Passover.jpeg",
+  HaShoah: "https://www.itu.org.il/wp-content/uploads/2022/01/%D7%99%D7%95%D7%9D-%D7%94%D7%96%D7%99%D7%9B%D7%A8%D7%95%D7%9F-%D7%94%D7%91%D7%99%D7%A0%D7%9C%D7%90%D7%95%D7%9E%D7%99-%D7%9C%D7%A9%D7%95%D7%90%D7%94-2022-300x300.jpeg",
+  HaZikaron: "https://educator.jewishedproject.org/sites/default/files/2021-03/ElementsPic%20YomHaZikaron%20Hayelet.gif",
+  HaAtzma: "https://mayersonjcc.org/app/uploads/2022/03/Yom-Image.jpg",
+  Yerushalayim: "https://i0.wp.com/www.touristisrael.com/wp-content/uploads/2020/05/Jerusalem-Day-scaled-1.jpg?resize=1920%2C1275&ssl=1",
+  Shavuot: "https://w2.chabad.org/media/images/960/jIsb9609528.jpg?_i=_nF6BA04C2A210B6E773D53EE06C920208",
+  Tish: "https://www-s3-live.kent.edu/s3fs-root/s3fs-public/file/Tisha%20Bav_0.jpg",
+  Rosh: "https://images.unsplash.com/photo-1582641924570-2330d8b224a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+  Kippur: "https://www.hebcal.com/i/is/800/1061644482.jpg",
+  Sukkot: "https://images.unsplash.com/photo-1571086180519-9c1fea323612?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  Atzeret: "https://cdn.bethamisrael.org/wp-content/uploads/2022/08/shemini-atzeret.jpeg",
+  Simchat: "https://images.unsplash.com/photo-1661919858163-6d56dc4bec97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1126&q=80",
+  Sigd: "https://images.jpost.com/image/upload/f_auto,fl_lossy/t_JD_ArticleMainImageFaceDetect/475543",
+  Chanukah: "https://images.unsplash.com/photo-1612375411938-6be39138a883?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+};
+
+export default ImageUrl 
--- holiday-app/src/Pages/MainPage.js
+++ holiday-app/src/Pages/MainPage.js
@@ -1,42 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import CardGrid from "../Components/CardGrid";
+import ImageUrl from "../HolidaysImages";
 
 export default function MainPage() {
   const [HolidaysInRange, setHolidaysInRange] = useState([]);
   const [StartDate, setStartDate] = useState("");
   const [EndDate, setEndDate] = useState("");
-  const [holidayImages, setHolidayImages] = useState({});
 
-  const getHolidayImages = async (holiday) => {
-    try {
-      const response = await axios.get(
-        `https://api.unsplash.com/search/photos?query=${holiday}&client_id=cceo9DOk_I2Pw-WpqHyXdhPxQto1rWwBysFmolMO5SQ`
-      );
-      return Object.values(response.data.results[0].urls.small).join("");
-    } catch (error) {
-      console.error(error);
+  function setImage(tmpImagesArray) {
+    for (let key in tmpImagesArray) {
+      for (let key2 in ImageUrl) {
+        if (tmpImagesArray[key].title.includes(key2)) {
+          tmpImagesArray[key]["ImageLink"] = ImageUrl[key2];
+        }
+      }
     }
-  };
-
-  useEffect(() => {
-    var tmpImagesArray = HolidaysInRange;
-    for (let key in HolidaysInRange) {
-      tmpImagesArray[key]["ImageLink"] = getHolidayImages(
-        tmpImagesArray[key].title
-      );
-    }
-    setHolidayImages(tmpImagesArray);
-    console.log("HolidaysInRange --> With Images: ", HolidaysInRange);
-    console.log("tmpImagesArray --> With Images: ", tmpImagesArray);
-  }, [HolidaysInRange]);
+    setHolidaysInRange(tmpImagesArray);
+  }
 
   const GetHebrewDates = async (startDate, endDate) => {
     // Assuming startDate & endDate are YYYY-MM-DD string formated
     try {
       const url = `https://www.hebcal.com/hebcal?v=1&mod=on&cfg=json&maj=on&start=${startDate}&end=${endDate}`;
       const response = await axios.get(url);
-      console.log("Holidays: ", response.data.items);
-      setHolidaysInRange(response.data.items);
+      setImage(response.data.items);
     } catch (error) {
       console.error(error);
     }
@@ -80,10 +68,14 @@ export default function MainPage() {
 
       {Object.keys(HolidaysInRange).length
         ? HolidaysInRange.map((item, ind) => (
-            <div key={ind}>
-              Holiday: {item.hebrew}, Date: {item.date} <br />
-              <img src={item.ImageLink} alt={item.title} />
-            </div>
+            <CardGrid
+              key={ind}
+              HebName={item.hebrew}
+              Date={item.date}
+              ImageLink={item.ImageLink}
+              EngName={item.title}
+              Info={item.memo}
+            />
           ))
         : "There are no holidays"}
       <br />
